da6df15ead3b8336ba8adbae9ac05447afe92e2d
wanglichaoya <wanglichaoya@163.com>
2020-03-01 17:59:33 +0800

    新增模拟回测 视图 网关

--- index-zuul-service/src/main/java/cn/how2j/trend/IndexZuulServiceApplication.java
+++ index-zuul-service/src/main/java/cn/how2j/trend/IndexZuulServiceApplication.java
@@ -3,6 +3,7 @@ package cn.how2j.trend;
 import cn.how2j.trend.util.CheckPortAbledUtil;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
@@ -15,6 +16,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @SpringBootApplication
 @EnableZuulProxy
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class IndexZuulServiceApplication {
     public static void main(String[] args){
         int port=8031;
