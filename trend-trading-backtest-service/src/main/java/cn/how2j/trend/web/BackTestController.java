8bb2b341faffbc049bd6c12282abf214e7d22133
wanglichaoya <wanglichaoya@163.com>
2020-03-04 13:53:24 +0800

    模拟回测-日期，切换日期，模拟回测图上的数据开始日期和结束日期会相应的改变

--- trend-trading-backtest-service/src/main/java/cn/how2j/trend/web/BackTestController.java
+++ trend-trading-backtest-service/src/main/java/cn/how2j/trend/web/BackTestController.java
@@ -2,15 +2,15 @@ package cn.how2j.trend.web;
 
 import cn.how2j.trend.pojo.IndexData;
 import cn.how2j.trend.service.BackTestService;
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.util.StrUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * describe:模拟回测控制器
@@ -23,14 +23,47 @@ public class BackTestController {
     @Autowired
     BackTestService backTestService;
 
-    @GetMapping("/simulate/{code}")
+    @GetMapping("/simulate/{code}/{startDate}/{endDate}")
     //@CrossOrigin跨域
     @CrossOrigin
-    public Map<String, Object> backTest(@PathVariable("code") String code) {
-        List<IndexData> indexDataList = backTestService.listIndexDate(code);
-        Map<String, Object> result = new HashMap<String, Object>();
-        result.put("indexDatas", indexDataList);
+    public Map<String, Object> backTest(@PathVariable("code") String code,
+                                        @PathVariable("startDate") String strStartDate,
+                                        @PathVariable("endDate") String strEndDate) {
+
+        List<IndexData> allIndexDatas = backTestService.listIndexDate(code);
+        //获取所有数据的第一条数据的开始时间
+        String indexStartDate = allIndexDatas.get(0).getDate();
+        String indexEndDate = allIndexDatas.get(allIndexDatas.size() - 1).getDate();
+        allIndexDatas = filterByDateRange(allIndexDatas, strStartDate, strEndDate);
+        Map<String, Object> result = new HashMap<>();
+        result.put("indexDatas", allIndexDatas);
+        result.put("indexStartDate", indexStartDate);
+        result.put("indexEndDate", indexEndDate);
         return result;
+
+    }
+
+    //对指定的日期的数据进行过滤
+    private List<IndexData> filterByDateRange(List<IndexData> allIndexDatas, String strStartDate, String strEndDate) {
+        //如果开始时间和结束时间都为空，则返回所有的数据
+        if (StrUtil.isBlankOrUndefined(strStartDate) || StrUtil.isBlankOrUndefined(strEndDate)) {
+            return allIndexDatas;
+        } else {
+            List<IndexData> indexDataList = new ArrayList<>();
+            //把日期类型的字符串转换为日期对象
+            Date startDate = DateUtil.parse(strStartDate);
+            Date endDate = DateUtil.parse(strEndDate);
+            for (IndexData indexData : allIndexDatas) {
+                Date date = DateUtil.parse(indexData.getDate());
+                //判断日期是否在开始和结束时间之间，如果是就放入到List集合中
+                if (date.getTime() >= startDate.getTime() && date.getTime() <= endDate.getTime()) {
+                    indexDataList.add(indexData);
+                }
+            }
+
+            return indexDataList;
+        }
+
 
     }
 }
